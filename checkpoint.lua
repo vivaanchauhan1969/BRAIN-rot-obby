b037263d706b285bb0d86d222f2804dad4f18de3
vivaanchauhan1969 <141391553+vivaanchauhan1969@users.noreply.github.com>
2024-08-11 12:45:00 -0500

    Update checkpoint.lua

--- checkpoint.lua
+++ checkpoint.lua
@@ -56,3 +56,33 @@ end)
 local trampoline = script.Parent
 trampoline.Velocity = Vector3.new(0, trampoline.Configuration.BounceSpeed.Value, 0)
 trampoline.SurfaceGui.Enabled = false	
+local debounce = false
+
+function getPlayer(humanoid) 
+local players = game.Players:children() 
+for i = 1, #players do 
+if players[i].Character.Humanoid == humanoid then return players[i] end 
+end 
+return nil 
+end 
+
+function onTouch(part) 
+
+local human = part.Parent:findFirstChild("Humanoid") 
+if (human ~= nil) and debounce == false then
+
+debounce = true
+
+local player = getPlayer(human) 
+
+if (player == nil) then return end 
+
+script.Parent:clone().Parent = player.Backpack
+
+wait(2)
+debounce = false
+end
+end
+
+
+script.Parent.Parent.Touched:connect(onTouch) 
