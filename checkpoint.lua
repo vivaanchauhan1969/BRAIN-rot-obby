c739b1054dadebfda7e06b053a0ceafcebf81b67
vivaanchauhan1969 <141391553+vivaanchauhan1969@users.noreply.github.com>
2024-08-11 12:43:48 -0500

    Update checkpoint.lua

--- checkpoint.lua
+++ checkpoint.lua
@@ -35,4 +35,24 @@ script.Parent.Touched:connect(function(hit)
 	if hit and hit.Parent and hit.Parent:FindFirstChild("Humanoid") then
 		hit.Parent.Humanoid.Health = 0
 	end
+local spawn = script.Parent
+spawn.Touched:connect(function(hit)
+	if hit and hit.Parent and hit.Parent:FindFirstChild("Humanoid") then
+		local player = game.Players:GetPlayerFromCharacter(hit.Parent)
+		local checkpointData = game.ServerStorage:FindFirstChild("CheckpointData")
+		if not checkpointData then
+			checkpointData = Instance.new("Model", game.ServerStorage)
+			checkpointData.Name = "CheckpointData"
+		end
+		
+		local checkpoint = checkpointData:FindFirstChild(tostring(player.userId))
+		if not checkpoint then
+			checkpoint = Instance.new("ObjectValue", checkpointData)
+			checkpoint.Name = tostring(player.userId)
+			
+			player.CharacterAdded:connect(function(character)
+				wait()
 end)
+local trampoline = script.Parent
+trampoline.Velocity = Vector3.new(0, trampoline.Configuration.BounceSpeed.Value, 0)
+trampoline.SurfaceGui.Enabled = false	
